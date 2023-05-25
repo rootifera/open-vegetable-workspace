81d0779e27951b83eec1b31cf106f462d78c0111
Omur Ozbahceliler <omur@rootifera.org>
2023-05-25 13:06:51 +0100

    test deploy

--- main.tf
+++ main.tf
@@ -97,11 +97,15 @@ resource "aws_lb" "hello_lb" {
     aws_subnet.public_subnet_1.id,
     aws_subnet.public_subnet_2.id,
   ]
-  subnet_mapping {
-    subnet_id = aws_subnet.public_subnet_1.id
-  }
-  subnet_mapping {
-    subnet_id = aws_subnet.public_subnet_2.id
+}
+
+resource "aws_lb_listener" "hello_lb_fe" {
+  load_balancer_arn = aws_lb.hello_lb.arn
+  port              = "80"
+  protocol          = "HTTP"
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.hello_tg.arn
   }
 }
 
